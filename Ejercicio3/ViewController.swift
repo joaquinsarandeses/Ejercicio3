4c741ebf5649d127543406f53d3234941e1415e4
joaquinsarandeses <joaquinsarandeses@gmail.com>
2022-10-06 22:28:37 +0200

    primer feature

--- Ejercicio3/ViewController.swift
+++ Ejercicio3/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController{
 
+    let arrayMuertos = [
+        Muertos(imageUrl: "http://cdn.shopify.com/s/files/1/0407/1168/4263/collections/thumbnail_image0_1024x.png?v=1650404960", title:"Tolkien", subtitle: "ayer"),
+        Muertos(imageUrl: "http://cdn.shopify.com/s/files/1/0407/1168/4263/collections/thumbnail_image0_1024x.png?v=1650404960", title:"Tolkien", subtitle: "ayer"),
+        Muertos(imageUrl: "http://cdn.shopify.com/s/files/1/0407/1168/4263/collections/thumbnail_image0_1024x.png?v=1650404960", title:"Tolkien", subtitle: "ayer"),
+        Muertos(imageUrl: "http://cdn.shopify.com/s/files/1/0407/1168/4263/collections/thumbnail_image0_1024x.png?v=1650404960", title:"Tolkien", subtitle: "ayer"),
+        Muertos(imageUrl: "http://cdn.shopify.com/s/files/1/0407/1168/4263/collections/thumbnail_image0_1024x.png?v=1650404960", title:"Tolkien", subtitle: "ayer"),
+        Muertos(imageUrl: "http://cdn.shopify.com/s/files/1/0407/1168/4263/collections/thumbnail_image0_1024x.png?v=1650404960", title:"Tolkien", subtitle: "ayer"),
+        Muertos(imageUrl: "http://cdn.shopify.com/s/files/1/0407/1168/4263/collections/thumbnail_image0_1024x.png?v=1650404960", title:"Tolkien", subtitle: "ayer")
+    
+    ]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayMuertos.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : Cell = tableView.dequeueReusableCell(withIdentifier:"Cell", for: indexPath) as! Cell
+        
+        let Title = arrayMuertos[indexPath.row].title
+        let SubTitle = arrayMuertos[indexPath.row].subtitle
+        let Image = getImageData(url:arrayMuertos[indexPath.row].imageUrl)
+        cell.Title.text = Title
+        cell.SubTitle.text = SubTitle
+        cell.Imagen.image = Image
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    func getImageData(url:String)->UIImage{
+        let url = URL(string: url)
+        let data = try? Data(contentsOf: url!)
+        var imageView: UIImage = UIImage(data: data!)!
+        return imageView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200;
+    }
 }
 
