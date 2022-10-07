4922bd66b7591153f82491a3c5c3605dda3d8c70
joaquinsarandeses <joaquinsarandeses@gmail.com>
2022-10-07 18:10:42 +0200

    Commit feature/ViewController

---
+++ Ejercicio3/Celda.swift
@@ -0,0 +1,23 @@
+//
+//  Celda.swift
+//  Ejercicio3
+//
+//  Created by joaquin sarandeses on 6/10/22.
+//
+
+import Foundation
+import UIKit
+
+class Cell:UITableViewCell{
+
+    @IBOutlet weak var Imagen: UIImageView!
+    @IBOutlet weak var Title: UILabel!
+    @IBOutlet weak var SubTitle: UILabel!
+    
+    
+    
+
+
+
+
+}
--- Ejercicio3/ViewController.swift
+++ Ejercicio3/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class TablaVC: UIViewController,UITableViewDataSource, UITableViewDelegate{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+
+    let arrayMuertos = [
+         Muertos(imageUrl: "http://cdn.shopify.com/s/files/1/0407/1168/4263/collections/thumbnail_image0_1024x.png?v=1650404960", title:"Tolkien", subtitle: "ayer"),
+         Muertos(imageUrl: "https://images.genius.com/24df596c2e657d773c3e6bc9477a1a25.1000x1000x1.jpg", title:"Tolkien", subtitle: "ayer"),
+         Muertos(imageUrl: "https://images.genius.com/24df596c2e657d773c3e6bc9477a1a25.1000x1000x1.jpg", title:"Tolkien", subtitle: "ayer"),
+         Muertos(imageUrl: "https://images.genius.com/24df596c2e657d773c3e6bc9477a1a25.1000x1000x1.jpg", title:"Tolkien", subtitle: "ayer"),
+         Muertos(imageUrl: "https://images.genius.com/24df596c2e657d773c3e6bc9477a1a25.1000x1000x1.jpg", title:"Tolkien", subtitle: "ayer"),
+         Muertos(imageUrl: "https://images.genius.com/24df596c2e657d773c3e6bc9477a1a25.1000x1000x1.jpg", title:"Tolkien", subtitle: "ayer"),
+         Muertos(imageUrl: "https://images.genius.com/24df596c2e657d773c3e6bc9477a1a25.1000x1000x1.jpg", title:"Tolkien", subtitle: "ayer")
+
+     ]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayMuertos.count
+    }
+    
+     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : Cell = tableView.dequeueReusableCell(withIdentifier:"cellID", for: indexPath) as! Cell
+
+        let Title = arrayMuertos[indexPath.row].title
+        let SubTitle = arrayMuertos[indexPath.row].subtitle
+        let Image = getImageData(url:arrayMuertos[indexPath.row].imageUrl)
+        cell.Title.text = Title
+        cell.SubTitle.text = SubTitle
+        cell.Imagen.image = Image
+        return cell
     }
 
+     override func viewDidLoad() {
+         tableView.dataSource = self
+         tableView.delegate = self
+         super.viewDidLoad()
+         
+         // Do any additional setup after loading the view.
 
-}
+     }
+    
+//fin
+     func getImageData(url:String)->UIImage{
+         let url = URL(string: url)
+         let data = try? Data(contentsOf: url!)
+         let imageView: UIImage = UIImage(data: data!)!
+         return imageView
+         
+     }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200;
+    }
+    
+    
+
+
+ }
 
